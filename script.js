92add3309082197fe3ea2241fa2fb79ff973f924
JuliNarvaez <0720jano@gmail.com>
2021-05-07 21:48:18 -0500

    JS refactor and chart implementation

--- script.js
+++ script.js
@@ -1,49 +1,93 @@
-var lorem = 0;
-generate();
+let lorem = 0;
+let chart;
+const SELECTOR = {
+    lorem: 'lorem',
+    paragraphs: '#paras',
+    textContainer: '.text',
+    countersContainer: '.counters',
+    wordCounter: '.word-counter',
+    charCounter: '.char-counter'
+};
 
 function checkedLorem() {
-    document.getElementById('lorem').checked ? lorem = 1 : lorem = 0;
+    const {checked:isLoremChecked} = document.getElementById(SELECTOR.lorem); 
+    lorem = isLoremChecked ? 1 : 0;
 }
 
-function generate() {
-    let paras = document.querySelector('#paras').value;
-    fetch(`https://baconipsum.com/api/?type=all-meat&paras=${paras}&start-with-lorem=${lorem}`)
+/* function generate() {
+    const paragraphs = document.querySelector(SELECTOR.paragraphs).value;
+    fetch(`https://baconipsum.com/api/?type=all-meat&paras=${paragraphs}&start-with-lorem=${lorem}`)
         .then(response => response.json())
-        .then(printText)
+        .then(handleResponse)
         .catch(err => console.log(err.message));
+} */
+
+async function generate() {
+    const paragraphs = document.querySelector(SELECTOR.paragraphs).value;
+    const link = `https://baconipsum.com/api/?type=all-meat&paras=${paragraphs}&start-with-lorem=${lorem}`
+    
+    try {
+        const response = await fetch(link);
+        const data = await response.json();
+        handleResponse(data);
+    } catch (error) {
+        console.error(error.message);
+    }    
 }
 
-function printText(text) {
-    const textCont = document.querySelector('.text');
-    textCont.innerHTML = text;
-    document.querySelector('.word-counter').innerHTML = `TOTAL WORDS: ${textCont.textContent.split(' ').length}`;
-    document.querySelector('.char-counter').innerHTML = `TOTAL CHARACTERS: ${textCont.textContent.length}`;
-    countRepeatedWords(textCont.textContent);
-    sortWords(textCont.textContent);
+
+function handleResponse(text) {
+    const textContainer = document.querySelector(SELECTOR.textContainer);
+
+    textContainer.innerHTML = text;
+    setCounterTexts(textContainer)
+    countRepeatedWords(textContainer.textContent);
+    sortWords(textContainer.textContent);
+}
+
+function setCounterTexts(textContainer){
+    const counters = document.querySelector(SELECTOR.countersContainer);
+
+    counters.querySelector(SELECTOR.wordCounter).innerHTML = `TOTAL WORDS: ${textContainer.textContent.split(' ').length}`;
+    counters.querySelector(SELECTOR.charCounter).innerHTML = `TOTAL CHARACTERS: ${textContainer.textContent.length}`;
 }
 
 function countRepeatedWords(allText) {
-    let text = allText.toLowerCase().replaceAll('.', '').replaceAll(',', '').replaceAll('  ', ' ');
-    let words = text.split(" ");
+    const text = allText.replaceAll(/[^\w\s]/gi, '').replaceAll('  ', ' ').toLowerCase();
+    const words = text.split(" ");
     let wordMap = {};
 
-    for (let i = 0; i < words.length; i++) {
-        let currentWordCount = wordMap[words[i]];
+    words.forEach(current => {
+        let currentWordCount = wordMap[current];
         let count = currentWordCount ? currentWordCount : 0;
-        wordMap[words[i]] = count + 1;
-    }
-    console.log(wordMap);
-    showFirstThree(sortWords(wordMap));
+        wordMap[current] = count + 1;
+    });
+
+    const topThree = getTopThree(sortWords(wordMap), wordMap);
+
+    renderChart(topThree);
 }
 
 function sortWords(wordMap) {
-    const keysSorted = Object.keys(wordMap).sort(function (a, b) {
-        return wordMap[b] - wordMap[a]
-    })
-    return keysSorted;
+    return Object.keys(wordMap).sort((current, previous) => wordMap[previous] - wordMap[current])
 }
 
-function showFirstThree(sortedWords) {
+function getTopThree(sortedWords, wordMap) {
     const ranking = sortedWords.slice(0, 3);
-    console.log(ranking);
+    return ranking.map(wordName => ([wordName, wordMap[wordName]]))
 }
+
+function renderChart(rows) {
+    anychart.onDocumentReady(function () {
+        const data = {
+            header: ["Name", "Total"],
+            rows
+        };
+        chart = chart || anychart.column()
+        chart.data(data);
+        chart.container("container");
+        chart.draw();
+    });
+}
+
+generate();
